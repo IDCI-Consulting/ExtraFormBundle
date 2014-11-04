0ac940dcd74d28d35755d757778d0166341e8b61
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-11-04 19:40:01 +0100

    Add JsonTextarea form type and ExtraFormType in order to display array in a textarea using json serialization

---
+++ Form/DataTransformer/JsonToArrayTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ *
+ */
+
+namespace IDCI\Bundle\ExtraFormBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+class JsonToArrayTransformer implements DataTransformerInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value)
+    {
+        if (null === $value) {
+            return "";
+        }
+
+        return json_encode($value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform($value)
+    {
+        return json_decode($value, true);
+    }
+}
+
+
---
+++ Form/Type/JsonTextareaType.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ *
+ */
+
+namespace IDCI\Bundle\ExtraFormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use IDCI\Bundle\ExtraFormBundle\Form\DataTransformer\JsonToArrayTransformer;
+
+class JsonTextareaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new JsonToArrayTransformer());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'textarea';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'json_textarea';
+    }
+}
