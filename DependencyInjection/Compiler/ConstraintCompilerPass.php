0dd3960ed1b22bf08cff48dc01751531e644e3f4
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-11-07 18:25:57 +0100

    Rename type and constraint using quote, rename form type

--- DependencyInjection/Compiler/ConstraintCompilerPass.php
+++ DependencyInjection/Compiler/ConstraintCompilerPass.php
@@ -19,5 +19,39 @@ class ConstraintCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
+        if (!$container->hasDefinition('idci_extra_form.constraint') ||
+            !$container->hasDefinition('idci_extra_form.constraint_handler')
+        ) {
+            return;
+        }
+
+        $handlerDefinition = $container->getDefinition('idci_extra_form.constraint_handler');
+
+        $constraints = $container->getParameter('idci_extra_form.constraints');
+        foreach ($constraints as $name => $configuration) {
+            $serviceDefinition = new DefinitionDecorator('idci_extra_form.constraint');
+            $serviceDefinition->replaceArgument(0, $configuration);
+
+            $container->setDefinition(
+                $this->getDefinitionName($name),
+                $serviceDefinition
+            );
+
+            $handlerDefinition->addMethodCall(
+                'setConstraint',
+                array($name, new Reference($this->getDefinitionName($name)))
+            );
+        }
+    }
+
+    /**
+     * Get definition name
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function getDefinitionName($name)
+    {
+        return sprintf('idci_extra_form.constraint.%s', $name);
     }
 }
