d614a2542ae5ede09105e19133accad0dd7a9278
Linok75 <anritot@gmail.com>
2015-07-29 21:25:33 +0200

    Update view form with select and add controller function

--- Resources/public/editor/editorApp.js
+++ Resources/public/editor/editorApp.js
@@ -1,5 +1,33 @@
 var editorApp = angular.module('editorApp', []);
 
-editorApp.controller('EditorController', function($scope) {
-    console.log('Todo');
-});
+editorApp.controller('EditorController', [ '$scope', '$http', function($scope, $http) {
+	$scope.json = {};
+	$scope.fieldtypes = {};
+	$scope.selectedFieldtype = undefined;
+	$scope.previewElement = angular.element($('#extraform-editor-preview'));
+
+    $scope.addField = function() {
+    	//Get html for "selectedFieldtype"
+    	$http.get("URI").
+    		success(function(data, status, headers, config) {
+    			$scope.previewElement.append(data);
+    			$scope.appendToJson(data);
+    		}).
+    		error(function(data, status, headers, config) {
+    			console.log(data);
+    		});
+    };
+
+    $scope.appendToJson = function(html) {
+    	$scope.json = {};
+    }
+
+    //Get field types from api
+    $http.get("URI").
+    	success(function(data, status, headers, config) {
+    		$scope.fieldtypes = data;
+    	}).
+    	error(function(data, status, headers, config)) {
+    		console.log(data);
+    	});
+}]);
