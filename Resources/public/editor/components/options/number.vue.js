de823ccf95fe7c9b0849f3b680387c4f6e45b73d
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-01-20 17:13:04 +0100

    Add the number option component

---
+++ Resources/public/editor/components/options/number.vue.js
@@ -0,0 +1,20 @@
+var numberOption = {
+
+  template:
+    '<div>' +
+      '<label :for="name">{{ name }}</label>' +
+      '<input :value="value" @input="updateOption($event.target.value)" type="number" :name="name">' +
+    '</div>'
+  ,
+
+  props: ['option', 'value', 'name'],
+
+  methods: {
+    updateOption: function(value) {
+      this.$emit('changed', {
+        'name': this.name,
+        'value': value
+      });
+    }
+  }
+};
