ac85e4031ef3b2ee94360bca9a6416ecdfc92a02
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-02-28 15:44:30 +0100

    Add missing uses

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,6 +7,8 @@
 
 namespace IDCI\Bundle\ExtraFormBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
