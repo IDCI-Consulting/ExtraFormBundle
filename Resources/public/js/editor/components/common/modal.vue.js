22a220ccfb60f3fb148bafa43a0c673760566375
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-05-10 11:44:43 +0200

    Identation details

--- Resources/public/js/editor/components/common/modal.vue.js
+++ Resources/public/js/editor/components/common/modal.vue.js
@@ -2,19 +2,19 @@
 Vue.component('modal', {
 
   template:
-      '<transition name="modal">' +
-          '<div class="vue-modal-mask">' +
-              '<div class="vue-modal-container">' +
-                  '<div class="vue-modal-header">' +
-                      '<slot name="header"></slot>' +
-                  '</div>' +
-                  '<div class="vue-modal-body">' +
-                      '<slot name="body"></slot>' +
-                  '</div>' +
-                  '<div class="vue-modal-footer">' +
-                      '<slot name="footer"></slot>' +
-                  '</div>' +
-              '</div>' +
+    '<transition name="modal">' +
+      '<div class="vue-modal-mask">' +
+        '<div class="vue-modal-container">' +
+          '<div class="vue-modal-header">' +
+            '<slot name="header"></slot>' +
           '</div>' +
-      '</transition>'
+          '<div class="vue-modal-body">' +
+            '<slot name="body"></slot>' +
+          '</div>' +
+          '<div class="vue-modal-footer">' +
+            '<slot name="footer"></slot>' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+    '</transition>'
 });
