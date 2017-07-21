b1efbc29bab40aac884f309129211277bb72a62a
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-07-21 15:47:47 +0200

    Fixes wrong priorities

--- Form/Type/ExtraFormEditorType.php
+++ Form/Type/ExtraFormEditorType.php
@@ -46,11 +46,11 @@ class ExtraFormEditorType extends AbstractType implements AssetProviderInterface
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $this->assetCollection->add(new Asset('IDCIExtraFormBundle:Form:extra_form_editor_assets.html.twig', array(), 1));
+        $this->assetCollection->add(new Asset('IDCIExtraFormBundle:Form:extra_form_editor_assets.html.twig', array(), 0));
         $this->assetCollection->add(new Asset('IDCIExtraFormBundle:Form:extra_form_editor_configuration.html.twig', array(
             'options' => $options,
             'form'    => $view
-        ), 0));
+        ), 1));
 
         $attrClass = 'extra-form-editor';
 
